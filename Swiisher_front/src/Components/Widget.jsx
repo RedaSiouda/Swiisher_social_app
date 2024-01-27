642fbab0da6d0f3e39774883008bc40fe5bfdff2
RedaSiouda <sioudareda.dev@gmail.com>
2024-01-27 16:27:42 -0500

    update

--- Swiisher_front/src/Components/Widget.jsx
+++ Swiisher_front/src/Components/Widget.jsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
-import quizPicData from './WidgetQuiz';
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import '../CSS/Widget.css';
 
-function Widget() {
-
-
+const Widget = () => {
   return (
-    <h3>hello wordl</h3>
-  )
+    <div className="widget">
+
+    <div className="widget__header">
+      <h2 className="widget__title">Discover more pets:</h2>
+    </div>
+    <br/>
+    <div className="widget__containTimeLine">
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="dog_rates"
+          options={{ height: 150 }}
+          className='widget__tweet'
+        />
+        <br/>
+        <br/>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="catsofinstagram"
+          options={{ height: 150 }}
+          className='widget__tweet'
+        />
+         
+
+    </div>
+    </div>
+  );
 }
 
 export default Widget;
---
+++ Swiisher_front/src/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='signUp'>
+      <div className="signUp__header">Swiisher</div>
+      <div className="signUp__containForm">
+        <form action="" className="signUp__form">
+          <input type="text" className="signUp__input" />
+          <input type="text" className="signUp__input" />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
